2b82f87d0cbaf0280425b46e46d3cc379007dcff
Omid Amini <oamini1994@gmail.com>
2023-07-03 13:25:35 +0200

    responsive update

--- src/App.jsx
+++ src/App.jsx
@@ -11,6 +11,7 @@ const App = () => {
   const handleSearch = (searchTerm) => {
     setLoading(true);
     setError(null);
+   
 
     fetch(`https://hn.algolia.com/api/v1/search?query=${searchTerm}`)
       .then((response) => response.json())
@@ -26,13 +27,17 @@ const App = () => {
   };
 
   return (
-    <div className="mx-auto mt-8 max-w-md">
-      <h1 className="mb-4 text-3xl font-bold">Hacker News Search</h1>
+    <div className="mx-auto mt-8 flex max-w-md flex-col items-center">
+      <h1 className="mb-8 font-sans text-3xl font-bold text-gray-100 sm:pt-8 ">
+        Hacker News Search
+      </h1>
       <Search onSearch={handleSearch} />
       {loading ? (
         <Spinner />
       ) : error ? (
-        <p className="mt-4 text-red-500">{error}</p>
+        <p className="mt-4 rounded bg-red-100 px-4 py-2 text-red-500">
+          {error}
+        </p>
       ) : (
         <SearchResults results={results} />
       )}
--- src/Components/SearchResults.jsx
+++ src/Components/SearchResults.jsx
@@ -3,14 +3,17 @@ import React from "react";
 
 const SearchResults = ({ results }) => {
   if (results.length === 0) {
-    return <p>No results found.</p>;
+    return <p className="text-white">No results found</p>;
   }
 
   return (
-    <ul className="mt-4">
+    <ul className="mt-8 flex flex-col items-center justify-center">
       {results.map((result) => (
         <li key={result.objectID}>
-          <a href={result.url} className="text-blue-600">
+          <a
+            href={result.url}
+            className="hover-sm:text-slate-50 hover-sm:bg-purple-300 cursor-pointer text-slate-50 hover:text-purple-300"
+          >
             {result.title}
           </a>
         </li>
